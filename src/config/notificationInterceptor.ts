fc0e70f6210aacdbd1acb9324c5a7af04566242d
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-10-29 19:04:27 +0300

    init commit

--- src/config/notificationInterceptor.ts
+++ src/config/notificationInterceptor.ts
@@ -16,6 +16,7 @@ export const notificationInterceptor = (error: AxiosError): Promise<AxiosError> 
       case 500:
       case 503:
       case 405:
+      case 403:
         messageKey = `common:common.error.http.${status}`;
         break;
       default:
--- src/reducers/notification.ts
+++ src/reducers/notification.ts
@@ -2,15 +2,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface NotificationState {
-  messageKey: string | undefined;
-  message: string | undefined;
-  variant: 'success' | 'danger' | 'info' | 'warning' | undefined;
+  messageKey?: string | undefined;
+  message?: string | undefined;
+  data?: any | undefined
+  variant?: 'success' | 'danger' | 'info' | 'warning' | undefined;
   show: boolean;
 }
 
 const initialState: NotificationState = {
   messageKey: undefined,
   message: undefined,
+  data: undefined,
   variant: "info",
   show: false,
 };
@@ -22,12 +24,14 @@ const notificationSlice = createSlice({
     showNotification: (state, action: PayloadAction<Omit<NotificationState, 'show'>>) => {
       state.messageKey = action.payload.messageKey;
       state.message = action.payload.message;
+      state.data = action.payload.data;
       state.variant = action.payload.variant;
       state.show = true;
     },
     hideNotification: (state) => {
       state.show = false;
       state.messageKey = undefined;
+      state.data = undefined;
       state.message = undefined;
       state.variant = undefined;
     },
