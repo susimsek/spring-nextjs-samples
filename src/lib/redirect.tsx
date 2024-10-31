6f2c334cda89ec10aec9f8bfca75a2c5217a74b8
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-10-31 20:46:10 +0300

    init commit

--- src/lib/redirect.tsx
+++ src/lib/redirect.tsx
@@ -1,45 +1,44 @@
+// redirect.tsx
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import languageDetector from './languageDetector';
 
-type RedirectProps = {
-  to?: string;
+// Helper function for language-based redirection
+const detectAndRedirect = (router, targetPath) => {
+  const detectedLng = languageDetector.detect() as string;
+
+  if (targetPath.startsWith('/' + detectedLng) && router.route === '/404') {
+    router.replace('/' + detectedLng + router.route);
+    return;
+  }
+
+  if (languageDetector.cache) {
+    languageDetector.cache(detectedLng);
+  }
+
+  router.replace('/' + detectedLng + targetPath);
 };
 
-// Custom hook to detect language and redirect
+// Custom hook for language-based redirection
 export const useRedirect = (to?: string): JSX.Element => {
   const router = useRouter();
   const targetPath = to || router.asPath;
 
-  // language detection and redirection logic
   useEffect(() => {
-    const detectedLng = languageDetector.detect() as string;
-
-    if (targetPath.startsWith('/' + detectedLng) && router.route === '/404') {
-      // prevent endless loop for 404 error pages
-      router.replace('/' + detectedLng + router.route);
-      return;
-    }
-
-    // Call cache only if it is defined
-    if (languageDetector.cache) {
-      languageDetector.cache(detectedLng);
-    }
-
-    router.replace('/' + detectedLng + targetPath);
+    detectAndRedirect(router, targetPath);
   }, [router, targetPath]);
 
   return <></>;
 };
 
-// Functional component for redirection
+// Functional components for direct usage
 export const Redirect: React.FC = () => {
   useRedirect();
   return <></>;
 };
 
+
 // Higher-order function for specific path redirection
-// eslint-disable-next-line react/display-name
 export const getRedirect = (to: string): React.FC => () => {
   useRedirect(to);
   return <></>;
