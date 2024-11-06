7c5a489b60c1f68c69a95d6f5a4c184ce392c56a
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-06 09:41:27 +0300

    init commit

--- src/main/java/io/github/susimsek/springnextjssamples/aspect/LoggingAspect.java
+++ src/main/java/io/github/susimsek/springnextjssamples/aspect/LoggingAspect.java
@@ -9,6 +9,14 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.util.StopWatch;
 
+/**
+ * Aspect for logging execution of service, repository, and controller beans.
+ * Logs entry, exit, and exception events for methods annotated with the {@link io.github.susimsek.springnextjssamples.config.logging.annotation.Loggable}
+ * annotation or within specific packages.
+ * <p>
+ * The aspect utilizes a {@link LoggingHandler} to perform the actual logging.
+ * Additionally, it measures the execution time of methods and logs it upon method exit.
+ */
 @Aspect
 @RequiredArgsConstructor
 @Slf4j
@@ -16,28 +24,46 @@ public class LoggingAspect {
 
     private final LoggingHandler loggingHandler;
 
+    /**
+     * Pointcut that matches methods annotated with {@link io.github.susimsek.springnextjssamples.config.logging.annotation.Loggable}
+     * or within classes annotated with {@link io.github.susimsek.springnextjssamples.config.logging.annotation.Loggable}.
+     * This is used to trigger logging on specific methods or classes.
+     */
     @Pointcut(
         "@within(io.github.susimsek.springnextjssamples.config.logging.annotation.Loggable) || "
             + "@annotation(io.github.susimsek.springnextjssamples.config.logging.annotation.Loggable)"
     )
     public void loggablePointcut() {
-        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+        // Pointcut marker method, implementations are in the advices.
     }
 
+    /**
+     * Pointcut that matches all methods within repository, service, and controller packages.
+     * This is used to apply logging across these application layers.
+     */
     @Pointcut(
         "within(io.github.susimsek.springnextjssamples.repository..*)"
             + " || within(io.github.susimsek.springnextjssamples.service..*)"
-            + " || within(io.github.susimsek.springnextjssamples.controller..*)"
+            + " || within(io.github.susimsek.springnextjssamples.web.controller..*)"
     )
     public void applicationPackagePointcut() {
-        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+        // Pointcut marker method, implementations are in the advices.
     }
 
+    /**
+     * Advice that logs method entry, exit, exceptions, and execution time.
+     * This advice is applied to methods that satisfy both the {@code applicationPackagePointcut()} and {@code loggablePointcut()} conditions.
+     *
+     * @param joinPoint the join point representing the method invocation
+     * @return the result of the method invocation
+     * @throws Throwable if the target method throws an exception
+     */
     @Around("applicationPackagePointcut() && loggablePointcut()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         if (loggingHandler.shouldNotMethodLog(joinPoint)) {
             return joinPoint.proceed();
         }
+
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
@@ -45,20 +71,21 @@ public class LoggingAspect {
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
 
-        // Log method entry
+        // Log method entry with parameters
         loggingHandler.logMethodEntry(className, methodName, args);
 
         try {
-            Object result = joinPoint.proceed();
+            Object result = joinPoint.proceed(); // Proceed with method execution
             stopWatch.stop();
             long duration = stopWatch.getTotalTimeMillis();
 
-            // Log method exit
+            // Log method exit with result and duration
             loggingHandler.logMethodExit(className, methodName, result, duration);
             return result;
         } catch (Throwable e) {
             stopWatch.stop();
             long duration = stopWatch.getTotalTimeMillis();
+            // Log exception with error message and duration
             loggingHandler.logException(className, methodName, args, e.getMessage(), duration);
             throw e;
         }
