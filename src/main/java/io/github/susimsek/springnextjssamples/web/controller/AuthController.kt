ab2c4ed05818a78c20ec048c1442b89d3a56a243
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-07 23:03:35 +0300

    init commit

--- src/main/java/io/github/susimsek/springnextjssamples/web/controller/AuthController.kt
+++ src/main/java/io/github/susimsek/springnextjssamples/web/controller/AuthController.kt
@@ -1,61 +1,83 @@
-package io.github.susimsek.springnextjssamples.web.controller;
+package io.github.susimsek.springnextjssamples.web.controller
 
-import io.github.susimsek.springnextjssamples.dto.request.LoginRequestDTO;
-import io.github.susimsek.springnextjssamples.dto.response.TokenDTO;
-import io.github.susimsek.springnextjssamples.service.AuthenticationService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.susimsek.springnextjssamples.dto.request.LoginRequestDTO
+import io.github.susimsek.springnextjssamples.dto.response.TokenDTO
+import io.github.susimsek.springnextjssamples.service.AuthenticationService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.MediaType
+import org.springframework.http.ProblemDetail
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Tag(name = "auth", description = "Authentication APIs")
 @RequestMapping("/api/v1/auth")
-@RequiredArgsConstructor
 @Validated
-public class AuthController {
-    private final AuthenticationService authenticationService;
+class AuthController(
+    private val authenticationService: AuthenticationService
+) {
 
-    @Operation(summary = "Authenticate user",
-        description = "Authenticate user and return access token")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Successfully authenticated",
-            content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = TokenDTO.class)) }),
-        @ApiResponse(responseCode = "400", description = "Invalid input",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                schema = @Schema(implementation = ProblemDetail.class))),
-        @ApiResponse(responseCode = "401", description = "Invalid login credentials",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                schema = @Schema(implementation = ProblemDetail.class))),
-        @ApiResponse(responseCode = "429", description = "Too Many Requests",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                schema = @Schema(implementation = ProblemDetail.class))),
-        @ApiResponse(responseCode = "500", description = "Internal server error",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                schema = @Schema(implementation = ProblemDetail.class)))
-    })
+    @Operation(
+        summary = "Authenticate user",
+        description = "Authenticate user and return access token"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200", description = "Successfully authenticated",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = TokenDTO::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "400", description = "Invalid input",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = ProblemDetail::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "401", description = "Invalid login credentials",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = ProblemDetail::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "429", description = "Too Many Requests",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = ProblemDetail::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "500", description = "Internal server error",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = ProblemDetail::class)
+                )]
+            )
+        ]
+    )
     @PostMapping("/token")
-    public ResponseEntity<TokenDTO> login(
+    fun login(
         @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "Login request",
-            required = true)
-        @Valid @RequestBody LoginRequestDTO loginRequest) {
-
-        TokenDTO tokenResponse = authenticationService.authenticate(loginRequest);
-
-        return ResponseEntity.ok(tokenResponse);
+            required = true
+        ) @Valid @RequestBody loginRequest: LoginRequestDTO
+    ): ResponseEntity<TokenDTO> {
+        val tokenResponse = authenticationService.authenticate(loginRequest)
+        return ResponseEntity.ok(tokenResponse)
     }
 }
--- src/main/java/io/github/susimsek/springnextjssamples/web/controller/HelloController.kt
+++ src/main/java/io/github/susimsek/springnextjssamples/web/controller/HelloController.kt
@@ -1,46 +1,50 @@
-package io.github.susimsek.springnextjssamples.web.controller;
+package io.github.susimsek.springnextjssamples.web.controller
 
-import io.github.susimsek.springnextjssamples.dto.response.HelloDTO;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Locale;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.susimsek.springnextjssamples.dto.response.HelloDTO
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.context.MessageSource
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @Tag(name = "hello", description = "Hello API")
 @SecurityRequirement(name = "bearerAuth")
-@RequiredArgsConstructor
-public class HelloController {
-
-    private final MessageSource messageSource;
+class HelloController(
+    private val messageSource: MessageSource
+) {
 
     @Operation(summary = "Say Hello", description = "Returns a simple Hello Next.js message in JSON format")
-    @ApiResponses(value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Successful response",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                schema = @Schema(implementation = HelloDTO.class))
-        ),
-        @ApiResponse(
-            responseCode = "500",
-            description = "Internal server error",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                schema = @Schema(example = "{\"error\": \"Internal Server Error\"}"))
-        )
-    })
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Successful response",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = HelloDTO::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(example = "{\"error\": \"Internal Server Error\"}")
+                )]
+            )
+        ]
+    )
     @GetMapping("/api/v1/hello")
-    public HelloDTO hello(Locale locale) {
-        var message =  messageSource.getMessage("hello.message", null, locale);
-        return new HelloDTO(message);
+    fun hello(locale: Locale): HelloDTO {
+        val message = messageSource.getMessage("hello.message", null, locale)
+        return HelloDTO(message)
     }
 }
