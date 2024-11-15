7736c3b7401b5a6beae15ace3e1f47f952dd1250
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-15 20:00:14 +0300

    init commit

--- src/main/desktop/renderer/preload.d.ts
+++ src/main/desktop/renderer/preload.d.ts
@@ -1,7 +1,15 @@
-import { IpcHandler } from '../main/preload';
+import { IpcRenderer } from 'electron';
 
 declare global {
   interface Window {
-    ipc: IpcHandler;
+    ipc: IpcRenderer & {
+      setLocale?: (locale: string) => void;
+      getLocale?: () => Promise<string | null>;
+      setTheme?: (theme: 'light' | 'dark') => void;
+      getTheme?: () => Promise<'light' | 'dark'>;
+      setToken?: (token: string) => Promise<void>;
+      getToken?: () => Promise<string | null>;
+      removeToken?: () => Promise<void>;
+    };
   }
 }
