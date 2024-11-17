cac138bc6d006a0471ea03b6cf1f4bc5bbf453ed
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-17 16:26:46 +0300

    init commit

--- src/main/desktop/renderer/components/UpdateNotifier.tsx
+++ src/main/desktop/renderer/components/UpdateNotifier.tsx
@@ -4,7 +4,7 @@ import { Button, Modal } from 'react-bootstrap';
 
 const UpdateNotifier = () => {
   const { t } = useTranslation('common'); // Use the 'common' namespace for translations
-  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(true);
 
   useEffect(() => {
     window.ipc?.onUpdateDownloaded?.(() => {
@@ -34,11 +34,11 @@ const UpdateNotifier = () => {
       </Modal.Header>
       <Modal.Body>{t('common:common.update.downloaded')}</Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          {t('common:common.cancel')}
-        </Button>
         <Button variant="primary" onClick={handleRestart}>
           {t('common:common.update.confirm')}
+        </Button>
+        <Button variant="secondary" onClick={handleClose}>
+          {t('common:common.update.remindLater')}
         </Button>
       </Modal.Footer>
     </Modal>
