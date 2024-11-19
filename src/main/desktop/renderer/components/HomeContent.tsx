bd20d2c4fef2399ce483865c9da24dc459f90f31
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-19 19:18:06 +0300

    init commit

--- src/main/desktop/renderer/components/HomeContent.tsx
+++ src/main/desktop/renderer/components/HomeContent.tsx
@@ -12,22 +12,24 @@ const HomeContent: React.FC<HomeContentProps> = ({ messageData, loading }) => {
   const { t } = useTranslation(['home', 'common']);
 
   return (
-    <div className="p-4">
-      <h1>{t('home:home.heading')}</h1>
-      <p style={{ color: 'var(--text-color)' }}>{t('home:home.description')}</p>
+    <>
+      <div className="p-4">
+        <h1>{t('home:home.heading')}</h1>
+        <p style={{ color: 'var(--text-color)' }}>{t('home:home.description')}</p>
 
-      <div className="my-3">
-        {loading ? (
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        ) : (
-          <p className="text-primary" style={{ fontWeight: 'bold' }}>
-            {messageData?.message}
-          </p>
-        )}
+        <div className="my-3">
+          {loading ? (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          ) : (
+            <p className="text-primary" style={{ fontWeight: 'bold' }}>
+              {messageData?.message}
+            </p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
