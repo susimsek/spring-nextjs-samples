fc000490a130adde717a891a20889fa04a12866b
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-19 22:24:23 +0300

    init commit

--- src/main/java/io/github/susimsek/springnextjssamples/constant/Constants.java
+++ src/main/java/io/github/susimsek/springnextjssamples/constant/Constants.java
@@ -8,6 +8,6 @@ public final class Constants {
 
     public static final String SYSTEM = "system";
 
-    public static final String SPRING_PROFILE_DEVELOPMENT = "local";
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
     public static final String SPRING_PROFILE_PRODUCTION = "prod";
 }
