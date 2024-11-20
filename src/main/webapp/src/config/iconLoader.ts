5a4dc142f960d7af4e36d685a3107e3423e81c0c
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-20 23:15:24 +0300

    init commit

--- src/main/webapp/src/config/iconLoader.ts
+++ src/main/webapp/src/config/iconLoader.ts
@@ -19,8 +19,22 @@ import {
   faTimes,
   faTrash,
 } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition, library } from '@fortawesome/fontawesome-svg-core';
 
+// Custom Sidebar Icon Definition
+const faSidebar: IconDefinition = {
+  prefix: 'fas',
+  iconName: 'sidebar',
+  icon: [
+    20,
+    20,
+    [],
+    '',
+    'M7 3h-5v14h5v-14zM9 3v14h9v-14h-9zM0 3c0-1.1 0.9-2 2-2h16c1.105 0 2 0.895 2 2v0 14c0 1.105-0.895 2-2 2v0h-16c-1.105 0-2-0.895-2-2v0-14zM3 4h3v2h-3v-2zM3 7h3v2h-3v-2zM3 10h3v2h-3v-2z', // SVG Path
+  ],
+};
+
+// Load icons into FontAwesome library
 export const loadIcons = () => {
   library.add(
     faGlobe,
@@ -46,5 +60,6 @@ export const loadIcons = () => {
     faExclamationTriangle,
     faInfoCircle,
     faTimes,
+    faSidebar,
   );
 };
