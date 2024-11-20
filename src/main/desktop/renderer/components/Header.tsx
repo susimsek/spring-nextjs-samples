aa33aa9286cc721663f27c438ef364419e61233f
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-20 22:49:37 +0300

    init commit

--- src/main/desktop/renderer/components/Header.tsx
+++ src/main/desktop/renderer/components/Header.tsx
@@ -44,7 +44,14 @@ const Header: React.FC<HeaderProps> = ({
       {/* Sidebar toggle icon - only visible if showSidebarToggle is true */}
       {showSidebarToggle && (
         <Button variant="link" onClick={onToggleSidebar ? onToggleSidebar : undefined} className="sidebar-toggler me-2">
-          <FontAwesomeIcon icon="bars" className="sidebar-toggler-icon" />
+          <svg
+            className="fa sidebar-toggler-icon"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M7 3h-5v14h5v-14zM9 3v14h9v-14h-9zM0 3c0-1.1 0.9-2 2-2h16c1.105 0 2 0.895 2 2v0 14c0 1.105-0.895 2-2 2v0h-16c-1.105 0-2-0.895-2-2v0-14zM3 4h3v2h-3v-2zM3 7h3v2h-3v-2zM3 10h3v2h-3v-2z"></path>
+          </svg>
         </Button>
       )}
 
