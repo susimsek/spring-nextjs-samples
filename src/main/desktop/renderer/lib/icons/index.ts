fc17e1bf33d0d70318caa0f9f6b810ca0f816128
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-21 08:51:34 +0300

    init commit

---
+++ src/main/desktop/renderer/lib/icons/index.ts
@@ -0,0 +1,13 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+export const faSidebar: IconDefinition = {
+  prefix: 'fas',
+  iconName: 'sidebar',
+  icon: [
+    20,
+    20,
+    [],
+    '',
+    'M7 3h-5v14h5v-14zM9 3v14h9v-14h-9zM0 3c0-1.1 0.9-2 2-2h16c1.105 0 2 0.895 2 2v0 14c0 1.105-0.895 2-2 2v0h-16c-1.105 0-2-0.895-2-2v0-14zM3 4h3v2h-3v-2zM3 7h3v2h-3v-2zM3 10h3v2h-3v-2z',
+  ],
+};
